2866b3986f6af805bcdf296913dcc3c8a7b6f3b6
mouislambd <tanhaislammou097@gmail.com>
2026-06-22 13:39:26 -0700

    fix authfilee

--- config/auth.js
+++ config/auth.js
@@ -1,46 +1,3 @@
-// import dotenv from "dotenv";
-// dotenv.config();
-
-// import { betterAuth } from "better-auth";
-// import { mongodbAdapter } from "better-auth/adapters/mongodb";
-// import { MongoClient } from "mongodb";
-
-// const client = new MongoClient(process.env.MONGODB_URI);
-// const db = client.db();
-
-// export const auth = betterAuth({
-//     database: mongodbAdapter(db),
-//     secret: process.env.BETTER_AUTH_SECRET,
-//     baseURL: process.env.BETTER_AUTH_URL,
-//     trustedOrigins: [
-//         process.env.CLIENT_URL,
-//         "https://bibliodrop-client-sand.vercel.app",
-//         "https://bibliodrop-client-git-main-tanhaislammou097-2792s-projects.vercel.app"
-//     ],
-//     emailAndPassword: {
-//         enabled: true,
-//     },
-//     socialProviders: {
-//         google: {
-//             clientId: process.env.GOOGLE_CLIENT_ID,
-//             clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//         },
-//     },
-//     session: {
-//         expiresIn: 60 * 60 * 24 * 7,
-//         updateAge: 60 * 60 * 24,
-//     },
-//     user: {
-//         additionalFields: {
-//             role: {
-//                 type: "string",
-//                 defaultValue: "user",
-//                 input: true,
-//             },
-//         },
-//     },
-// });
-
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -55,7 +12,20 @@ export const auth = betterAuth({
     database: mongodbAdapter(db),
     secret: process.env.BETTER_AUTH_SECRET,
     baseURL: process.env.BETTER_AUTH_URL,
-    trustedOrigins: [process.env.CLIENT_URL],
+    trustedOrigins: [
+        process.env.CLIENT_URL,
+        "https://bibliodrop-client-sand.vercel.app",
+        "https://bibliodrop-client-git-main-tanhaislammou097-2792s-projects.vercel.app"
+    ],
+    advanced: {
+        crossSubDomainCookies: {
+            enabled: false,
+        },
+        defaultCookieAttributes: {
+            sameSite: "none",
+            secure: true,
+        }
+    },
     emailAndPassword: {
         enabled: true,
     },
@@ -76,16 +46,6 @@ export const auth = betterAuth({
                 defaultValue: "user",
                 input: true,
             },
-        },
-    },
-    advanced: {
-        crossSubDomainCookies: {
-            enabled: false,
-        },
-        defaultCookieAttributes: {
-            sameSite: "none",
-            secure: true,
-            partitioned: true,
         },
     },
 });
